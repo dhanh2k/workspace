2af5e446250b3236ce8e956ffb0b5adaa6dd7b00
dhanh2k <dhanh2k@gmail.com>
2024-09-13 10:23:59 +0700

    13/9/24

--- chat/routes/userRoute/users.js
+++ chat/routes/userRoute/users.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
+let refreshTokens = []
+
 router.get("/", authenticateToken, async (req, res) => {
     try {
         const users = await User.find()
@@ -61,6 +63,7 @@ router.post("/login", async (req, res) => {
                 console.log(user)
                 const accessToken = generateAccessToken(user)
                 const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECKET)
+                refreshTokens.push(refreshToken)
                 res.json({ accessToken, refreshToken })
             } else {
                 res.json({ message: "not allowed" })
@@ -73,20 +76,41 @@ router.post("/login", async (req, res) => {
     }
 })
 
+router.post("/token", (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECKET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken(user)
+        res.json({ accessToken: accessToken })
+    })
+})
+
+router.delete("/logout", (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
+    console.log(token)
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
+        if (err){
+            console.log(err)
+            return res.sendStatus(403)
+        } 
+            
         req.user = user
         next()
     })
 }
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s' })
 }
 
 module.exports = router
